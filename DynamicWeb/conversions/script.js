b15f1ea96043cbe0be5eb7e80ded8cd4bb233e42
marvinuofa <mcu@ualberta.ca>
2025-05-14 08:41:17 -0600

    added distance and weight conversions

--- DynamicWeb/conversions/script.js
+++ DynamicWeb/conversions/script.js
@@ -2,6 +2,20 @@ function cel_to_fahrenheit(){
     var cel = document.getElementById("celsius");
     var fahrenheit = document.getElementById("fahrenheit");
 
-    fahrenheit.value = (cel.value* 9/5) + 32;
-   
+    fahrenheit.value = (cel.value* 9/5) + 32;  
+}
+
+function km_to_miles(){
+    var km = document.getElementById("kilometers");
+    var miles = document.getElementById("miles");
+
+    miles.value = km.value * 0.62137;
+
+}
+
+function kg_to_lbs(){
+    var kg = document.getElementById("kilograms");
+    var lbs = document.getElementById("pounds");
+
+    lbs.value = kg.value * 2.2;
 }
