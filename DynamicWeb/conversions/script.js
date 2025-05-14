9a66a3ec577ea1291177337dc675a275c2df1104
marvinuofa <mcu@ualberta.ca>
2025-05-14 02:05:19 -0600

    celsius to fahrenheit complete

--- DynamicWeb/conversions/script.js
+++ DynamicWeb/conversions/script.js
@@ -1,7 +1,7 @@
 function cel_to_fahrenheit(){
-    var cel = document.getElementById("celcius");
+    var cel = document.getElementById("celsius");
     var fahrenheit = document.getElementById("fahrenheit");
 
-    fahrenheit.value = (cel.value* 9/5) + 32
-
+    fahrenheit.value = (cel.value* 9/5) + 32;
+   
 }
